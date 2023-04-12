b1fce601e0a4eb5c2f0c510f3d8a0d0f88d042ee
elina <elina.paxar@gmail.com>
2023-04-12 14:29:37 +0300

    add exit with error in func

--- src/funcs_for_time.cpp
+++ src/funcs_for_time.cpp
@@ -1,8 +1,13 @@
+#include <iostream>
 #include "funcs_for_time.h"
 
 hh_mm parse_time(const std::string& str, char sep) {
     auto sep_pos = str.find(sep);
     auto block_1 = str.substr(0, sep_pos);
+    if (block_1.size() == str.size()) {
+        std::cout << "separator symbol error";
+        exit(1);
+    }
     auto block_2 = str.substr(sep_pos + 1);
 
     return {block_1, block_2};
