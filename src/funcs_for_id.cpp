376dd05d14a72001fe8ab334f1cced7fbed92fca
elina <elina.paxar@gmail.com>
2023-04-13 02:08:43 +0300

    fix bugs

--- src/funcs_for_id.cpp
+++ src/funcs_for_id.cpp
@@ -7,15 +7,15 @@ void id11(hh_mm& time, const std::string& name) {
     std::cout << 11 << ' ' << name << '\n';
 }
 
-void id12(club_info& work_info, hh_mm& time, std::string& name, size_t index) {
+void id12(club_info& work_info, hh_mm& time, size_t index) {
     if (work_info.queue_clients.empty()) {
         return;
     }
 
     print_time(time, false);
-    std::cout << 12 << ' ' << name << index + 1 << '\n';
+    std::cout << 12 << ' ' << work_info.queue_clients[0] << ' ' << index + 1 << '\n';
+    work_info.who_sits[index] = work_info.queue_clients[0];
     work_info.queue_clients.erase(work_info.queue_clients.cbegin());
-    work_info.who_sits[index] = name;
 }
 
 void id13(hh_mm& time, const std::string& error_msg) {
@@ -44,8 +44,9 @@ void id1(club_info& work_info, hh_mm& time, std::string& name) {
 void id2(club_info& work_info, hh_mm& time, std::string& name, uint16_t table_num) {
     const auto find_name = [name](const auto queue) { return std::find(std::cbegin(queue), std::cend(queue), name); };
 
-    if (find_name(work_info.who_sits) != std::cend(work_info.who_sits)) {
-        work_info.who_sits.erase(find_name(work_info.who_sits));
+    if (find_name(work_info.who_sits) == std::cend(work_info.who_sits)) {
+        size_t index = std::distance(std::cbegin(work_info.who_sits), find_name(work_info.who_sits));
+        work_info.who_sits[index] = "";
     }
     if (find_name(work_info.queue_clients) == std::cend(work_info.queue_clients)) {
         id13(time, "ClientUnknown");
@@ -88,5 +89,5 @@ void id4(club_info& work_info, hh_mm& time, std::string& name) {
     auto table = std::find(std::cbegin(work_info.who_sits), std::cend(work_info.who_sits), name);
     size_t index = std::distance(std::cbegin(work_info.who_sits), table);
     work_info.who_sits[index] = "";
-    id12(work_info, time, name, index);
+    id12(work_info, time, index);
 }
